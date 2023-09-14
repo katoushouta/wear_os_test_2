fbafe392700aa4c7ed5aaac1f67d9409834ae0f5
katoushouta <shouta.katou@link-u.co.jp>
2023-09-14 23:44:42 +0900

    update: 心拍数計測

--- wear/src/main/java/com/example/wear_os_test_2/presentation/MainActivity.kt
+++ wear/src/main/java/com/example/wear_os_test_2/presentation/MainActivity.kt
@@ -10,6 +10,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -22,8 +24,17 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.health.services.client.HealthServices
+import androidx.health.services.client.HealthServicesClient
+import androidx.health.services.client.MeasureCallback
+import androidx.health.services.client.MeasureClient
+import androidx.health.services.client.data.Availability
+import androidx.health.services.client.data.DataPointContainer
 import androidx.health.services.client.data.DataType
+import androidx.health.services.client.data.DataTypeAvailability
+import androidx.health.services.client.data.DeltaDataType
+import androidx.health.services.client.data.SampleDataPoint
 import androidx.health.services.client.getCapabilities
+import androidx.health.services.client.unregisterMeasureCallback
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -33,23 +44,97 @@ import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import com.example.wear_os_test_2.R
 import com.example.wear_os_test_2.presentation.theme.Wear_os_test_2Theme
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    lateinit var healthClient: HealthServicesClient
+    lateinit var measureClient: MeasureClient
+    @ExperimentalCoroutinesApi
+    fun heartRateMeasureFlow() = callbackFlow {
+        val callback = object : MeasureCallback {
+            override fun onAvailabilityChanged(dataType: DeltaDataType<*, *>, availability: Availability) {
+                // Only send back DataTypeAvailability (not LocationAvailability)
+                if (availability is DataTypeAvailability) {
+                    Log.d("DEBUG", "availability: $availability")
+                    trySendBlocking(availability)
+                }
+            }
+
+            override fun onDataReceived(data: DataPointContainer) {
+                val heartRateBpm = data.getData(DataType.HEART_RATE_BPM)
+                Log.d("DEBUG", "BPM: ${heartRateBpm.last().value}")
+                trySendBlocking(heartRateBpm)
+            }
+        }
+
+        Log.d("DEBUG", "Registering for data")
+        measureClient.registerMeasureCallback(DataType.HEART_RATE_BPM, callback)
+
+        awaitClose {
+            Log.d("DEBUG", "Unregistering for data")
+            runBlocking {
+                measureClient.unregisterMeasureCallback(DataType.HEART_RATE_BPM, callback)
+            }
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val healthClient = HealthServices.getClient(this)
-        val measureClient = healthClient.measureClient
+        Log.d("FULL", "start")
+        healthClient = HealthServices.getClient(this /*context*/)
+        measureClient  = healthClient.measureClient
+
+        permissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            Log.d("FULL", "when")
+            when (result) {
+                true -> {
+                    Log.i("DEBUG", "Body sensors permission granted")
+                    // Only measure while the activity is at least in STARTED state.
+                    // MeasureClient provides frequent updates, which requires increasing the
+                    // sampling rate of device sensors, so we must be careful not to remain
+                    // registered any longer than necessary.
+                    lifecycleScope.launchWhenStarted {
+                        heartRateMeasureFlow().collect {
+                            when (it) {
+                                is Availability -> {
+                                    Log.d("DEBUG", "Availability changed: ${it}")
+                                }
+                                is List<*> -> {
+                                    val bpm = it.last()
+                                    Log.d("DEBUG", "Data update: $bpm")
+                                }
+                            }
+                        }
+                    }
+                }
+                false -> Log.i("DEBUG", "Body sensors permission not granted")
+            }
+        }
         var supportsHeartRate = false
         lifecycleScope.launch {
             val capabilities = measureClient.getCapabilities()
             supportsHeartRate = DataType.HEART_RATE_BPM in capabilities.supportedDataTypesMeasure
-            Log.d("DEBUD", supportsHeartRate.toString())
+            Log.d("DEBUG", supportsHeartRate.toString())
         }
 
         setContent {
             WearApp(supportsHeartRate.toString())
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        permissionLauncher.launch(android.Manifest.permission.BODY_SENSORS)
     }
 }
 
