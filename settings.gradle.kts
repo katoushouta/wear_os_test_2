38f9606cfcd82b5f123895071e2dac59184ba279
katoushouta <shouta.katou@link-u.co.jp>
2023-09-14 20:27:43 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "wear_os_test_2"
+include(":mobile")
+include(":wear")
+ 
