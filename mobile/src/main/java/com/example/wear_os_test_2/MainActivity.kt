d9d152a7bcc037c37420842921f21798dc640fbe
katoushouta <shouta.katou@link-u.co.jp>
2023-10-12 22:31:24 +0900

    add: MobileAppに心拍数表示
    add: MobileAppに心拍数を表示

--- mobile/src/main/java/com/example/wear_os_test_2/MainActivity.kt
+++ mobile/src/main/java/com/example/wear_os_test_2/MainActivity.kt
@@ -7,12 +7,18 @@ import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.DataEventBuffer
 import com.google.android.gms.wearable.DataMapItem
@@ -21,12 +27,14 @@ import com.google.android.gms.wearable.Wearable
 private const val FROM_WEAR_PATH = "/heart_rate_bpm"
 private const val VITAL_KEY = "com.example.wear_test_2.vital"
 class MainActivity : AppCompatActivity(), DataClient.OnDataChangedListener {
+    private val vitalData = mutableStateOf("--")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { MobileApp() }
+        setContent { MobileApp(vitalData) }
     }
 
     override fun onDataChanged(dataEvents: DataEventBuffer) {
+        Log.d("DEBUG", "on data change")
         dataEvents.forEach { it ->
             val uri = it.dataItem.uri
             val nodeId = uri.host
@@ -38,6 +46,7 @@ class MainActivity : AppCompatActivity(), DataClient.OnDataChangedListener {
                 if ((item.uri.path?.compareTo(FROM_WEAR_PATH) ?: -1) == 0) {
                     DataMapItem.fromDataItem(item).dataMap.apply {
                         Toast.makeText(applicationContext, getInt(VITAL_KEY).toString(), Toast.LENGTH_SHORT).show()
+                        vitalData.value = getInt(VITAL_KEY).toString()
                     }
                 }
             }
@@ -50,17 +59,23 @@ class MainActivity : AppCompatActivity(), DataClient.OnDataChangedListener {
     }
 
     override fun onPause() {
+        Log.d("DEBUG", "onPause")
         super.onPause()
         Wearable.getDataClient(this).removeListener(this)
     }
 }
 
 @Composable
-fun MobileApp() {
+fun MobileApp(vitalData: MutableState<String>) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
         Text("Hello Wear", textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Text("心拍数")
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(vitalData.value)
+        }
     }
 }
